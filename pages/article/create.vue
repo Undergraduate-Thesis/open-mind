0d0c3ed29445e78a337866f490583ef1c8fff8c4
victory chrisnanto <vkrisna65@gmail.com>
2020-09-08 12:46:53 +0700

    add page create article

---
+++ pages/article/create.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Create Article</h1>
+    <form action="POST">
+      <input type="text" name="title" id="title" />
+      <textarea name="detail" id="detail" cols="30" rows="10"></textarea>
+    </form>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  mounted() {
+    console.log(this.$route.params);
+  },
+});
+</script>
