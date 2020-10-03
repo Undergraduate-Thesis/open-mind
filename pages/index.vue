8ecd3cb501bb1a14635d2b3c255bbe8027a5064a
victory chrisnanto <vkrisna65@gmail.com>
2020-10-03 23:42:59 +0700

    create main layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,94 @@
 <template>
   <div class="container">
+    <!--
+  Tailwind UI components require Tailwind CSS v1.8 and the @tailwindcss/ui plugin.
+  Read the documentation to get started: https://tailwindui.com/documentation
+-->
     <div>
-      <router-link
-        to="/Article/create"
-        tag="button"
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-      >Create Article</router-link>
+      <nav class="bg-gray-800">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div class="flex items-center justify-between h-16">
+            <div class="flex items-center">
+              <div class="flex-shrink-0">
+                <img
+                  class="h-8 w-8"
+                  src="https://tailwindui.com/img/logos/workflow-mark-on-dark.svg"
+                  alt="Workflow logo"
+                />
+              </div>
+            </div>
+            <div class="hidden md:block">
+              <div class="ml-4 flex items-center md:ml-6">
+                <nuxt-link
+                  to="/404"
+                  tag="button"
+                  class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
+                >
+                  Log in
+                </nuxt-link>
+                <nuxt-link
+                  to="/404"
+                  tag="button"
+                  class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+                >
+                  Create account
+                </nuxt-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 class="text-3xl font-bold leading-tight text-gray-900">
+            Dashboard
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <!-- Replace with your content -->
+          <div class="px-4 py-6 sm:px-0">
+            <div class="px-16 py-4 text-gray-700 bg-gray-200 h-screen w-screen">
+              <!-- Content -->
+              <cardArticle
+                v-for="article in articles"
+                :key="article._id"
+                :article="article"
+              />
+            </div>
+          </div>
+          <!-- /End replace -->
+        </div>
+      </main>
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from "vue";
+import cardArticle from "@/components/Card-Article";
 
 export default Vue.extend({
+  components: {
+    cardArticle,
+  },
+  data() {
+    return {
+      articles: [],
+    };
+  },
   mounted() {
-    this.$myInjectedFunction("works in mounted");
+    // this.$myInjectedFunction("works in mounted");
+    this.$axios
+      .get("http://localhost:3000/api/v1/article")
+      .then((res) => {
+        this.articles = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 });
 </script>
