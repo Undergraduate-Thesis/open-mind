57ce2bb1d4163fe85395e8fe2e5390ea496a25de
victory chrisnanto <vkrisna65@gmail.com>
2020-10-04 21:07:21 +0700

    create login page

---
+++ pages/auth/login.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="login">
+    <!--
+  Tailwind UI components require Tailwind CSS v1.8 and the @tailwindcss/ui plugin.
+  Read the documentation to get started: https://tailwindui.com/documentation
+-->
+    <div
+      class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8"
+    >
+      <div class="max-w-md w-full">
+        <div>
+          <img
+            class="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg"
+            alt="Workflow"
+          />
+          <h2
+            class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900"
+          >
+            Sign in to your account
+          </h2>
+        </div>
+        <form class="mt-8" action="#" method="POST">
+          <input type="hidden" name="remember" value="true" />
+          <div class="rounded-md shadow-sm">
+            <div>
+              <input
+                aria-label="Email address"
+                aria-required="true"
+                aria-invalid="true"
+                name="email"
+                type="email"
+                required
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Email address"
+                v-model="email"
+              />
+            </div>
+            <div class="-mt-px">
+              <input
+                name="password"
+                type="password"
+                aria-label="Password"
+                aria-invalid="true"
+                aria-describedby="errorMessage"
+                aria-required="true"
+                required
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Password"
+                v-model="password"
+              />
+            </div>
+          </div>
+          <div v-if="errorMessage != ''" class="px-2 py-2 text-red-500">
+            <p class="errorMessage">{{ errorMessage }}</p>
+          </div>
+
+          <div class="mt-6">
+            <button
+              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+              @click.prevent="Sigin()"
+            >
+              <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                <svg
+                  class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      email: "",
+      password: "",
+      errorMessage: "",
+    };
+  },
+  methods: {
+    Sigin(email, password) {
+      this.errorMessage = "";
+      this.$axios
+        .post("http://localhost:3000/api/v1/auth/login", {
+          email: this.email,
+          password: this.password,
+        })
+        .then((res) => {
+          this.$router.push({ path: "/" });
+        })
+        .catch((err) => {
+          this.errorMessage = err.response.data;
+        });
+    },
+  },
+});
+</script>
