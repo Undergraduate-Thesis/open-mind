fa5662e5dd60d4e14c5eae43aa91e03144f825ad
victory chrisnanto <vkrisna65@gmail.com>
2020-10-04 21:12:48 +0700

    add page create article

--- pages/article/create.vue
+++ pages/article/create.vue
@@ -1,13 +1,29 @@
 <template>
-  <div class="container">
-    <h1>Create Article</h1>
-    <form action="POST">
-      <input type="text" name="title" id="title" />
-      <textarea name="detail" id="detail" cols="30" rows="10"></textarea>
-    </form>
+  <div class="wrapper">
+    <header class="items">
+      <h1>Create Article</h1>
+    </header>
+    <div class="items contents">
+      <div class="article__header">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >Add a cover image</button>
+      </div>
+      <div class="article__content">
+        <textarea class="rounded-lg" name="detail" id="detail" cols="30" rows="10">
+Write your post content here...
+        </textarea>
+      </div>
+      <div class="article__footer">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >Add a cover image</button>
+      </div>
+    </div>
+    <footer class="items --text-xs">By Victory Chrisnanto Que</footer>
   </div>
 </template>
-<script>
+<script lang="ts">
 import Vue from "vue";
 export default Vue.extend({
   mounted() {
