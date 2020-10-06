e572c0e957da071d2c998175429f216fc75a9065
victory chrisnanto <vkrisna65@gmail.com>
2020-10-06 21:13:51 +0700

    create register page

---
+++ pages/auth/register.vue
@@ -0,0 +1,327 @@
+<template>
+  <div class="login">
+    <!--
+  Tailwind UI components require Tailwind CSS v1.8 and the @tailwindcss/ui plugin.
+  Read the documentation to get started: https://tailwindui.com/documentation
+-->
+    <div
+      class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8"
+    >
+      <div class="max-w-md w-full">
+        <div>
+          <svg
+            fill="none"
+            viewBox="0 0 24 24"
+            class="mx-auto h-12 w-auto text-blue-600"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+            />
+          </svg>
+          <h2
+            class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900"
+          >
+            Join with us!
+          </h2>
+        </div>
+        <form
+          class="w-full max-w-xl mt-8 bg-white rounded-lg shadow-md p-6"
+          action="#"
+          method="POST"
+          @submit.prevent="signup()"
+        >
+          <input type="hidden" name="remember" value="true" />
+          <div class="rounded-md shadow-sm">
+            <div class="mb-6">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="name"
+                >name</label
+              >
+              <input
+                name="name"
+                type="text"
+                aria-label="name"
+                aria-invalid="true"
+                aria-describedby="errorMessage"
+                aria-required="true"
+                required
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Name"
+                v-model="name"
+              />
+            </div>
+            <div class="mb-6">
+              <label
+                class="uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="gender"
+                >Gender</label
+              >
+              <div class="relative">
+                <select
+                  v-model="gender"
+                  class="block appearance-none w-full border border-gray-300 px-4 py-2 pr-8 rounded focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                >
+                  <option disabled value="">Please select one</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
+                <div
+                  class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500"
+                >
+                  <svg
+                    class="fill-current h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div class="mb-6">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="email"
+                >Email Address</label
+              >
+              <input
+                aria-label="Email address"
+                aria-required="true"
+                aria-invalid="true"
+                name="email"
+                type="email"
+                required
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Email address"
+                v-model="email"
+              />
+            </div>
+            <div class="mb-6">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="password"
+                >Password</label
+              >
+              <input
+                name="password"
+                type="password"
+                aria-label="Password"
+                aria-invalid="true"
+                aria-required="true"
+                required
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Password"
+                v-model="password"
+              />
+            </div>
+            <div class="mb-2">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="password"
+                >Password Confirm</label
+              >
+              <input
+                name="passwordConfirm"
+                type="password"
+                aria-label="Password Confirm"
+                aria-invalid="true"
+                aria-required="true"
+                required
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Password Confirm"
+                v-model="passwordConfirm"
+              />
+            </div>
+          </div>
+
+          <!-- <div v-if="errorMessage != ''" class="px-2 py-2 text-red-500">
+            <p class="errorMessage">{{ errorMessage }}</p>
+          </div> -->
+          <div class="flex justify-start mt-3 ml-4 p-1">
+            <ul>
+              <li class="flex items-center py-1">
+                <div
+                  :class="{
+                    'bg-green-200 text-green-700':
+                      password == passwordConfirm && password.length > 0,
+                    'bg-red-200 text-red-700':
+                      password != passwordConfirm || password.length == 0,
+                  }"
+                  class="rounded-full p-1 fill-current"
+                >
+                  <svg
+                    class="w-4 h-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      v-show="
+                        password == passwordConfirm && password.length > 0
+                      "
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M5 13l4 4L19 7"
+                    />
+                    <path
+                      v-show="
+                        password != passwordConfirm || password.length == 0
+                      "
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </div>
+                <span
+                  :class="{
+                    'text-green-700':
+                      password == passwordConfirm && password.length > 0,
+                    'text-red-700':
+                      password != passwordConfirm || password.length == 0,
+                  }"
+                  class="font-medium text-sm ml-3"
+                  v-text="
+                    password == passwordConfirm && password.length > 0
+                      ? 'Passwords match'
+                      : 'Passwords do not match'
+                  "
+                ></span>
+              </li>
+              <li class="flex items-center py-1">
+                <div
+                  :class="{
+                    'bg-green-200 text-green-700': password.length >= 8,
+                    'bg-red-200 text-red-700': password.length < 8,
+                  }"
+                  class="rounded-full p-1 fill-current"
+                >
+                  <svg
+                    class="w-4 h-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      v-show="password.length >= 8"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M5 13l4 4L19 7"
+                    />
+                    <path
+                      v-show="password.length < 8"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </div>
+                <span
+                  :class="{
+                    'text-green-700': password.length >= 8,
+                    'text-red-700': password.length < 8,
+                  }"
+                  class="font-medium text-sm ml-3"
+                  v-text="
+                    password.length >= 8
+                      ? 'The minimum length is reached'
+                      : 'At least 8 characters required'
+                  "
+                ></span>
+              </li>
+            </ul>
+          </div>
+          <div v-show="emailExists == true" class="flex items-center py-1">
+            <div class="bg-red-200 text-red-700 rounded-full p-1 fill-current">
+              <svg
+                class="w-4 h-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </div>
+            <span class="text-red-700 font-medium text-sm ml-3"
+              >Email already exists</span
+            >
+          </div>
+
+          <div class="mt-6">
+            <button
+              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+              type="submit"
+            >
+              <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                <svg
+                  class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+              Sign up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      name: "",
+      gender: "male",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+      emailExists: false,
+    };
+  },
+  methods: {
+    signup() {
+      this.errorMessage = "";
+      this.$axios
+        .post("http://localhost:3000/api/v1/auth/register", {
+          name: this.name,
+          gender: this.gender,
+          email: this.email,
+          password: this.password,
+        })
+        .then((res) => {
+          this.$router.push({ path: "/" });
+        })
+        .catch((err) => {
+          if (err.response.data == "Email already exists") {
+            this.emailExists = true;
+          }
+          console.log(err.response.data);
+        });
+    },
+  },
+});
+</script>
