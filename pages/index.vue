5127e3932b66b2dc449018867115b7d980e8a58b
vkrisna65 <vkrisna65@gmail.com>
2021-01-04 17:27:20 +0700

    add search function

--- pages/index.vue
+++ pages/index.vue
@@ -10,8 +10,8 @@
           <!-- This is an example component -->
           <div class="pt-2 relative text-gray-600">
             <input
-              v-model="search"
-              class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+              v-model="textSearch"
+              class="border-2 z-0 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
               type="search"
               name="search"
               placeholder="Search"
@@ -129,7 +129,7 @@ export default Vue.extend({
       currentTime: "",
       articles: [],
       tags: [],
-      search: "",
+      textSearch: "",
     };
   },
   async mounted() {
@@ -165,9 +165,12 @@ export default Vue.extend({
         this.currentTime = "Good Evening";
       }
     },
-    search() {
+    async search() {
       try {
-        this.articles = this.$axios.$get("/search", { text: this.search });
+        this.articles = await this.$axios.$post("/article/search", {
+          text: this.textSearch,
+        });
+        console.log(this.articles);
       } catch (error) {
         console.log("error", error);
       }
