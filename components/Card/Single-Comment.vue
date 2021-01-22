73224ae4fab76a781401bcf211a2130cc0bde0d4
vkrisna65 <vkrisna65@gmail.com>
2021-01-22 21:59:11 +0700

    modify comment

--- components/Card/Single-Comment.vue
+++ components/Card/Single-Comment.vue
@@ -10,42 +10,47 @@
       />
       <div class="w-full">
         <div class="flex items-center justify-between">
-          <h2 class="text-lg font-semibold text-gray-900 -mt-1">{{ comment.userName }}</h2>
-            <button
-                v-if="user != null && user.id == comment.userId"
-                class="bg-red-700 text-gray-100 px-3 py-1 font-semibold text-sm rounded"
-                @click="deleteComment"
-              >
-              Delete
-              </button>
+          <h2 class="text-lg font-semibold text-gray-900 -mt-1">
+            {{ comment.userName }}
+          </h2>
+          <button
+            v-if="user != null && user.id == comment.userId"
+            class="bg-red-700 text-gray-100 px-3 py-1 font-semibold text-sm rounded"
+            @click="deleteComment"
+          >
+            Delete
+          </button>
         </div>
-        <p class="text-gray-700">{{comment.created_at}}</p>
+        <p class="text-gray-700">{{ comment.created_at }}</p>
         <p class="mt-3 text-gray-700 text-sm">
           {{ comment.comment }}
         </p>
-        </div>
       </div>
     </div>
   </div>
+  <!-- </div> -->
 </template>
 <script>
 export default {
   props: {
-    comment: {},
+    comment: {}
   },
   data() {
     return {
       user: JSON.parse(localStorage.getItem("user"))
-    }
+    };
   },
   mounted() {
-      const created_at = new Date(this.comment.created_at);
-      this.comment.created_at = `${created_at.getDate()} ${created_at.toLocaleString('default', { month: 'short' })}`;
+    const created_at = new Date(this.comment.created_at);
+    this.comment.created_at = `${created_at.getDate()} ${created_at.toLocaleString(
+      "default",
+      { month: "short" }
+    )}`;
   },
   methods: {
     deleteComment() {
-      this.$emit("deleteComment")
+      this.$emit("deleteComment");
     }
-  },
+  }
 };
 </script>
