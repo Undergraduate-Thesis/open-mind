44ae3fde89ae0a8fdf16f01f64981ee767d8779a
vkrisna65 <vkrisna65@gmail.com>
2021-02-15 19:13:20 +0700

    create page validation

--- pages/validation/_id.vue
+++ pages/validation/_id.vue
@@ -1,68 +1,78 @@
 <template>
   <div id="validation">
-    <table class="table-fixed">
-      <thead>
-        <tr>
-          <th class="w-4/6">Article</th>
-          <th class="w-1/6">Summary</th>
-          <th class="w-1/6">Validation</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(sentence, index) in content" :key="index">
-          <td class="border-2 border-green-400">{{ sentence }}</td>
-          <td
-            v-if="summary.includes(index)"
-            class="text-center border-2 border-green-400"
-          >
-            True
-          </td>
-          <td v-else class="text-center border-2 border-green-400"></td>
-          <td
-            v-if="summary.includes(index)"
-            class="text-center border-2 border-green-400"
-          >
-            <div class="flex justify-between lg:px-12">
-              <label class="inline-flex items-center">
-                <input
-                  type="radio"
-                  :name="index"
-                  :id="index"
-                  value="True"
-                  class="form-radio h-4 w-4 text-indigo-400"
-                /><span class="ml-2 text-gray-700">True</span>
-              </label>
-              <label class="inline-flex items-center">
-                <input
-                  type="radio"
-                  :name="index"
-                  :id="index"
-                  value="False"
-                  class="form-radio h-4 w-4 text-indigo-400"
-                /><span class="ml-2 text-gray-700">False</span>
-              </label>
-            </div>
-          </td>
-          <td v-else class="text-center border-2 border-green-400"></td>
-        </tr>
-      </tbody>
-    </table>
+    <form id="myForm" @submit.prevent="validation($event)">
+      <table class="table-fixed">
+        <thead>
+          <tr>
+            <th class="w-4/6">Article</th>
+            <th class="w-1/6">Summary</th>
+            <th class="w-1/6">Validation</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(sentence, index) in content" :key="index">
+            <td class="border-2 border-green-400">{{ sentence }}</td>
+            <td
+              v-if="summary.includes(index)"
+              class="text-center border-2 border-green-400"
+            >
+              True
+            </td>
+            <td v-else class="text-center border-2 border-green-400"></td>
+            <td
+              v-if="summary.includes(index)"
+              class="text-center border-2 border-green-400"
+            >
+              <div class="flex justify-between lg:px-12">
+                <label class="inline-flex items-center">
+                  <input
+                    type="radio"
+                    :name="index"
+                    :id="index"
+                    value="True"
+                    class="form-radio h-4 w-4 text-indigo-400"
+                  /><span class="ml-2 text-gray-700">True</span>
+                </label>
+                <label class="inline-flex items-center">
+                  <input
+                    type="radio"
+                    :name="index"
+                    :id="index"
+                    value="False"
+                    class="form-radio h-4 w-4 text-indigo-400"
+                  /><span class="ml-2 text-gray-700">False</span>
+                </label>
+              </div>
+            </td>
+            <td v-else class="text-center border-2 border-green-400"></td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="flex justify-center">
+        <button
+          class="text-white text-center bg-blue-700 w-1/2 py-2 my-4 text-sm font-medium text-left bg-transparent rounded-md inline focus:outline-none"
+        >
+          Submit Bosque
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
+      title: "",
       content: [],
       summary: []
     };
   },
   async mounted() {
     const article = await this.$axios.$get(`/article/${this.$route.params.id}`);
+    this.title = article.title;
     this.content = article.content.split(".");
 
     const summary = article.summary.split(".");
-    console.log(summary);
 
     this.content.forEach((contentSentence, i) => {
       const sentenceWIthoutSpace = contentSentence.replace(" ", "");
